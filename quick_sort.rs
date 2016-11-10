ae6126ec6a9ef241b150a588f61293866aa44aef
Eduardo Ismael <eduardo78d@gmail.com>
2016-11-09 21:41:19 -0600

    quick_sort

--- quick_sort.rs
+++ quick_sort.rs
@@ -1,3 +1,39 @@
+fn quick_sort(vector: Vec<i32>) -> Vec<i32> {
+	if vector.len() > 1{
+		let mut less = Vec::new();
+		let mut equals = Vec::new();
+		let mut greater = Vec::new();
+
+		let pivote = vector[0];
+
+		for value in vector{
+
+			if value > pivote{
+				greater.push(value)
+			}
+			else if value < pivote{
+				less.push(value)
+			}
+			else{
+				equals.push(value)
+			}
+		}
+
+		let mut final_vector = Vec::new();
+		final_vector.extend(quick_sort(less));
+		final_vector.extend(quick_sort(equals));
+		final_vector.extend(quick_sort(greater));
+
+		return final_vector;
+
+	}else{
+		return vector
+	}
+}
+
+
 fn main() {
-	println!("Hello world!");
+	let vector = vec![9,7,18,29,5, 3,99, 77];
+	let result = quick_sort(vector);
+	println!("{:?}", result );
 }
