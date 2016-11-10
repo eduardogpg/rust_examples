aa91248898a64cf8925f2d40c458a905d670cc02
Eduardo Ismael <eduardo78d@gmail.com>
2016-11-09 22:46:08 -0600

    refactor

--- quick_sort.rs
+++ quick_sort.rs
@@ -11,17 +11,17 @@ fn quick_sort(vector: Vec<i32>) -> Vec<i32> {
 			if value > pivote{
 				greater.push(value)
 			}
-			else if value < pivote{
+			if value < pivote{
 				less.push(value)
 			}
-			else{
+			if value == pivote{
 				equals.push(value)
 			}
 		}
 
 		let mut final_vector = Vec::new();
 		final_vector.extend(quick_sort(less));
-		final_vector.extend(quick_sort(equals));
+		final_vector.extend(equals);
 		final_vector.extend(quick_sort(greater));
 
 		return final_vector;
@@ -33,7 +33,7 @@ fn quick_sort(vector: Vec<i32>) -> Vec<i32> {
 
 
 fn main() {
-	let vector = vec![9,7,18,29,5, 3,99, 77];
+	let vector = vec![9,7,18,29,5,3,99,77,8,1,19,199,13,15,1];
 	let result = quick_sort(vector);
 	println!("{:?}", result );
 }
